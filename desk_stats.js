89689214ec47d6c0a0162d6f2a583b86ee120c96
n9i5a <leah.belyea@unb.ca>
2015-03-04 13:51:05 -0400

    edits to form

--- desk_stats.js
+++ desk_stats.js
@@ -1,53 +1,58 @@
 (function(jQuery) {
   jQuery.fn.setUpForm = function() {
     // add radio category label
-    jQuery('label[for="edit-inquiry-complex-reference-5-15"]').parent('div').before('<p class="radio-label">Basic Reference</p>');
-    // show time input when override is selected
-    jQuery('input[name="override"]').change(function() {
-      if (jQuery('input[name="override"]').prop('checked')) {
-        jQuery('div.timestamp').removeClass('hide');
-      }
-      else {
-        jQuery('div.timestamp').addClass('hide');
+    jQuery('input[value="Basic Reference"]').parent('label').parent('div').before('<p class="subgroup-label">Reference</p>');
+
+    // add class to checkboxes in subgroup
+    jQuery('.inquiry input[type="checkbox"]').parent('label').parent('div').slice(4,8).addClass('subgroup');
+
+    // move student and faculty/staff types to beneath the appropriate radio button
+    jQuery('.student-type').detach().insertAfter(jQuery('input[name="patron"][value="Student"]').parent('label'));
+    jQuery('.staff-type').detach().insertAfter(jQuery('input[name="patron"][value="Faculty/Staff"]').parent('label'));
+
+    // remove extra asterisks added by form api states
+    jQuery('input[name="override"]').click(function() {
+      var labels = jQuery('.container-inline-date').find('label');
+      for (var i=0; i<labels.length; i++) {
+        jQuery(labels[i]).find('span.form-required').slice(1).remove();
       }
     });
   };
 })(jQuery);
 
 (function(jQuery) {
-  jQuery.fn.resetResponder = function(location) {
-    jQuery('select[name="responder"]').val('Research Help Desk');
-  };
-})(jQuery);
-
-(function(jQuery) {
-  jQuery.fn.resetFields = function(selectDefaults, radioDefaults, textfieldDefaults) {
-    selectDefaults = JSON.parse(selectDefaults);
+  jQuery.fn.resetFields = function(radioDefaults, textfieldDefaults) {
     radioDefaults = JSON.parse(radioDefaults);
     textfieldDefaults = JSON.parse(textfieldDefaults);
 
-    for (var field in selectDefaults) {
-      jQuery('select[name="' + field + '"]').val(selectDefaults[field]);
-    }
-
     for (var field in radioDefaults) {
       if(radioDefaults[field] == '') {
-        jQuery('input[name="' + field + '"]').prop('checked', false);
+        if (field == 'patron') {
+          jQuery('input[name="' + field + '"]').click();
+        }
+        else {
+          jQuery('input[name="' + field + '"]').prop('checked', false);
+        }
       }
       else {
-        jQuery('input[name="' + field + '"][value="' + radioDefaults[field] + '"]').prop('checked', true);
+        if (field == 'patron') {
+          jQuery('input[name="' + field + '"][value="' + radioDefaults[field] + '"]').click();
+        }
+        else {
+          jQuery('input[name="' + field + '"][value="' + radioDefaults[field] + '"]').prop('checked', true);
+        }
       }
     }
+
+    jQuery('.inquiry input[type="checkbox"]').prop('checked', false);
 
     for (var field in textfieldDefaults) {
       jQuery('input[name="' + field + '"]').val(textfieldDefaults[field]);
     }
-    hideTimestamp();
+
+    if (jQuery('input[name="override"]').prop('checked')) {
+      jQuery('input[name="override"]').click();
+    }
+
   };
 })(jQuery);
-
-function hideTimestamp() {
-  jQuery('input[name="override"]').prop('checked', false);
-  jQuery('div.timestamp').addClass('hide');
-}
-
